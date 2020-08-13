f07b992c369dbce3190d9235fc58923453c6e7ab
DRed <dima.redkin.1999@gmail.com>
2020-08-13 16:33:22 +0500

    Добавил порты для UdpSender в конфиг (appsettings.json). Проект ServiceOne.

--- MainService/Middleware/UdpSender.cs
+++ MainService/Middleware/UdpSender.cs
@@ -2,16 +2,23 @@
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace MainService.Middleware
 {
     public class UdpSender : IRequestSender
     {
-        private string _host = "127.0.0.1";
+        private readonly string _host = "127.0.0.1";
 
-        private readonly int _portForStartedRequest = 7003;
-        private readonly int _portForFinishedRequest = 7004;
+        private readonly int _portForStartedRequest;
+        private readonly int _portForFinishedRequest;
+
+        public UdpSender(IConfiguration configuration)
+        {
+            _portForStartedRequest = int.Parse(configuration["portForStartedRequest_Udp"]);
+            _portForFinishedRequest = int.Parse(configuration["portForFinishedRequest_Udp"]);
+        }
 
         public async Task SendStartedRequest(Dictionary<string, string> content)
         {
--- ServiceOne/Startup.cs
+++ ServiceOne/Startup.cs
@@ -1,7 +1,6 @@
 using MainService.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
