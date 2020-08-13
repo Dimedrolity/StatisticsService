4c8be18bf8a1b54e3235304efe39c93869422c8a
DRed <dima.redkin.1999@gmail.com>
2020-08-13 13:03:06 +0500

    Добавил обработку незавершенных запросов, на случай, если будет утерян udp-пакет об окончании запроса.

--- MainService/RequestsCollector.cs
+++ MainService/RequestsCollector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Threading.Tasks;
 using MainService.Requests;
 
 namespace MainService
@@ -9,10 +10,34 @@ namespace MainService
         public ConcurrentDictionary<string, UnfinishedRequest> UnfinishedRequests { get; }
         public ConcurrentBag<FinishedRequest> FinishedRequests { get; }
 
+        private readonly long _maxRequestTimeInMilliseconds = 60 * 60 * 1000;
+        private readonly int _frequencyOfFinishingOldRequests = 5 * 60 * 1000;
+
         public RequestsCollector()
         {
             UnfinishedRequests = new ConcurrentDictionary<string, UnfinishedRequest>();
             FinishedRequests = new ConcurrentBag<FinishedRequest>();
+
+            Task.Run(FinishOldRequests);
+        }
+
+        private async Task FinishOldRequests()
+        {
+            while (true)
+            {
+                var now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+                foreach (var (guid, request) in UnfinishedRequests)
+                {
+                    var requestStartTime = request.StartTimeInMilliseconds;
+                    var isRequestOld = now - requestStartTime > _maxRequestTimeInMilliseconds;
+
+                    if (isRequestOld)
+                        UnfinishedRequests.TryRemove(guid, out var dummy);
+                }
+
+                await Task.Delay(_frequencyOfFinishingOldRequests);
+            }
         }
 
         public void SaveStartedRequest(string guid, string method, string url, long startTime)
